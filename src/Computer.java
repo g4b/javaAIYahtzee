2665fc55a078545791ff167a531d3d0a9a9db861
Gabe <gabrielfantacone@students.berkeley.net>
2020-01-24 10:26:45 -0800

    fix threeKind, go over numRolls

--- src/Computer.java
+++ src/Computer.java
@@ -7,6 +7,7 @@ public class Computer extends Player {
     public void computerRoll(boolean signal){
         if (signal){
             System.out.println(this.name + "'s turn! " + this.name + " is rolling...");
+            System.out.println(this.name + "'s current roll: " + this.playerDie.getStringRolls(this.playerDie.getNumRolls()));
             this.playerDie.calcScore(this.playerDie.getNumRolls(), false);
         } else {
             System.out.println(this.name + " died. Please reload and try again.");
